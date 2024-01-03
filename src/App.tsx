7cf5b8affe1983438f322b206781134dfe6b3cb1
nedoratic <nedoratic@gmail.com>
2024-01-03 22:16:34 +0100

    markdown functionality added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-// import { useState } from 'react';
+import { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 import './App.css';
 
 function App() {
-	return <></>;
+	const [markdownText, setMarkdownText] = useState<string>('');
+
+	return (
+		<>
+			<div>
+				<h1>Markdown Previewer</h1>
+				<div>
+					<div>
+						<h2>Editor</h2>
+						<textarea name="editor" id="editor" value={markdownText} onChange={(e) => setMarkdownText(e.target.value)}></textarea>
+					</div>
+					<div id="preview">
+						<h2>Previewer</h2>
+						<ReactMarkdown>{markdownText}</ReactMarkdown>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
